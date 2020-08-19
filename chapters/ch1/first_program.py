20609ee58bbdf247c387d4d25669d25617ad88fb
Carlos Kidman <ckcarlosdesign@gmail.com>
2020-08-19 12:47:15 -0600

    Challenge 1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,8 @@
+# 1. Ask user for their first name
+first_name = input('Enter your first name: ')
+
+# 2. Ask user for their last name
+last_name = input('Enter your last name: ')
+
+# 3. Print their full name
+print(f'Your full name is {first_name} {last_name}')
