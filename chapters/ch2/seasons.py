2f1a48809f13bbc3388e6bc6db2426785be2cfcd
Carlos Kidman <ckcarlosdesign@gmail.com>
2020-08-22 21:59:28 -0600

    Challenge 2

---
+++ chapters/ch2/seasons.py
@@ -0,0 +1,13 @@
+""" Challenge 2
+
+Write a program, with Scenarios and Tests, that does the following:
+
+1. Ask the user to enter a Season
+2. Using this input, create the logic to handle different seasons:
+
+    * If the input is "winter", print "snow"
+    * If the input is "spring", print "flowers"
+    * If the input is "summer", print "beach"
+    * If the input is "fall" or "autumn", print "leaves"
+    * If the input is anything else, print "I don't know that season"
+"""
